042193b419c7379370f294b97028c477f59fcbeb
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2019-12-30 22:57:22 -0800

    Update Average.js

--- Average.js
+++ Average.js
@@ -1,2 +1,13 @@
+/*
+This will return the average of an array after mapping the individual elements to a value.
+
+Array.prototype.map() maps each element to the value returned by fn.
+
+Array.prototype.reduce() adds each value to an accumulator starting with the value of 0 then divide by the length of the array.
+*/
+
+
 const averageBy = (arr, fn) =>
+{  
   arr.map(typeof fn === 'function' ? fn : val => val[fn]).reduce((acc, val) => acc + val, 0) / arr.length;
+}
