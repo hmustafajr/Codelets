db07e5a4f67c0cf453a68d2ea73575bdc6a08cca
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-01-05 14:37:30 -0800

    Update Frequency.js

--- Frequency.js
+++ Frequency.js
@@ -1,2 +1,8 @@
 //returns the number of times a character is found in an array
 
+const frequency = arr =>
+arr.reduce((a, v) =>
+           { a[v] = a[v] ? a[v] + 1 : 1;
+            return a;
+           },
+{});
