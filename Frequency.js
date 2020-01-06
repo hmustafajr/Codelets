f6d962c3eb799193124d3768455771e7845a4fec
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-01-05 16:21:04 -0800

    Update Frequency.js

--- Frequency.js
+++ Frequency.js
@@ -3,7 +3,7 @@
 const frequency = arr =>
 arr.reduce((a, v) =>
 {
-           a[v] = a[v] ? a[v] + 1 : 1;
+           a[v] = a[v] ? a[v] + 1 : 1; //?s are used as conditional operators that assign a value to a variable based on a condition
            return a;
 },
 {});
