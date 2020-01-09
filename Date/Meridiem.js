01b42b94b21629d59093ed356565bdc3b7aede8b
Hassan (mufasa) <47394208+hmustafajr@users.noreply.github.com>
2020-01-08 22:32:28 -0800

    Update Meridiem.js

--- Date/Meridiem.js
+++ Date/Meridiem.js
@@ -1,14 +1,14 @@
 /*
-Converts an integer to a suffixed string, adding am or pm based on its value.
-
-Use the modulo operator (%) and conditional checks to transform an integer to a stringified 12-hour format with meridiem suffix.
+Converts an int to a suffixed string
+adding AM or PM based on the value
+Uses the modulo operator (%) and conditional checks to transform an integer to a stringified 12-hour format with meridiem suffix
 */
 
 const getMeridiemIntSuff = num =>
   num === 0 || num === 24
-    ? 12 + 'am'
+    ? 12 + 'AM'
     : num === 12
-      ? 12 + 'pm'
+      ? 12 + 'PM'
       : num < 12
-        ? (num % 12) + 'am'
-        : (num % 12) + 'pm';
+        ? (num % 12) + 'AM'
+        : (num % 12) + 'PM';
